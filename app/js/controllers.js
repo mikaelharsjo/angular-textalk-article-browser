029b4842649a7033e3f094ebfca04190f16e4ebc
Mikael Härsjö <mikaelharsjo@gmail.com>
2014-05-28 23:30:02 +0200

    Fetch article groups and display as menu

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,26 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
 
+  .controller('MyCtrl2', ['$scope', function($scope) {
+  }])
+
+  .controller('ArticleGroupCtrl', ['$scope', function ($scope) {
+  	$scope.articleGroups = []
+    var client = new $.JsonRpcClient({ socketUrl: 'ws://shop.textalk.se/backend/jsonrpc/v1?webshop=11011' });
+    var articleGroupFetched = function(group) {
+    	$scope.$apply(function(){
+    		$scope.articleGroups.push(group);
+    	});
+    }
+    var articleGroupIdsFetched = function(ids) {
+      angular.forEach(ids, function(groupId) {
+        client.call('Articlegroup.get', [groupId], articleGroupFetched);	
+      }); 
+    }
+
+    client.call('Assortment.getArticlegroupUids', [], articleGroupIdsFetched); 	
   }]);
+
+
